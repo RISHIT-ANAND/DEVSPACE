7d2dfaa4fa86fb6deeba4f0b81f27815d8d29495
Md Rushd Al Amin <72799488+vitmdrushdalamin@users.noreply.github.com>
2021-03-20 10:52:34 +0530

    Create get api.js

---
+++ frontend/get api.js
@@ -0,0 +1,17 @@
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
+
+var client = new HttpClient();
+client.get('http://some/thing?with=arguments', function(response) {
+    // do something with response
+});
