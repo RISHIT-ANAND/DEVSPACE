e4e4e1141fe5cbb90652994e4959ce98c2e9bbb0
VITAmin <mdrushd.alamin2020@vitstudent.ac.in>
2021-03-20 20:20:19 +0530

    del

--- frontend/src/js/sidebar-collapse.js
+++
@@ -1,5 +0,0 @@
-$('[data-toggle="collapse"]').on('click', function () {
-  $('.drawer .sidebar-menu .open').removeClass('open');
-  $(this).parent().toggleClass('open');
-  $(this).next('collapse').toggleClass('show');
-});
