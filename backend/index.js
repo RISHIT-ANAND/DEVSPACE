ee6c836e23093e7f1aaf3ee440d60855afa955ca
thesct22 <sharathct22@gmail.com>
2021-03-20 23:58:34 +0530

    Added minor tweaks and error handling

--- backend/index.js
+++ backend/index.js
@@ -20,23 +20,23 @@ var transporter = nodemailer.createTransport({
 var mysql      = require('mysql');
 var connection = mysql.createPool({
     host     : 'remotemysql.com',
-    user     : 'cc4mxFSMWm',
-    password : '8NmwE8H5Sh',
-    database : 'cc4mxFSMWm'
+    user     : 'Uiz0hUNUje',
+    password : 'k24nEVIJ78',
+    database : 'Uiz0hUNUje'
 });
 //connection.connect();
 var sendres=null;
 
 
 schedule.scheduleJob('0 0 * * *', () => { 
-    connection.query('select agreementid, vendorname, email from Agreement left join Vendor on Agreement.vendorid=Vendor.vendorid where Vendor.vendorid in (select vendorid from Agreement where productid in(select productid from Agreement where datediff(expirydate,curdate())<15))', function (error, results, fields) {
+    connection.query('select vpid, vendorname, email from vp left join Vendor on vp.vendorid=Vendor.vendorid where Vendor.vendorid in (select vendorid from vp where productid in(select productid from Agreement where datediff(expirydate,curdate())<15))', function (error, results, fields) {
         if (error) throw error;
         var arr=results.map((val,index)=>{
             var mailOptions = {
                 from: 'procurementassistantvit21@gmail.com',
                 to: val["email"],
                 subject: 'Hello'+val["vendorname"],
-                text: 'Please click this link to view our initial prices https://proc-assist.herokuapp.com/'+val["agreementid"]
+                text: 'Please click this link to view our initial prices https://proc-assist.herokuapp.com/'+val["vpid"]
             };  
             transporter.sendMail(mailOptions, function(error, info){
             if (error) {
@@ -50,7 +50,7 @@ schedule.scheduleJob('0 0 * * *', () => {
         sendres=results;
     }); 
 })
-connection.query('SELECT vendorname, email from Vendor where vendorid in (select vendorid from Agreement where productid in(select productid from Agreement where datediff(expirydate,curdate())<15))', function (error, results, fields) {
+connection.query('select vpid, vendorname, email from vp left join Vendor on vp.vendorid=Vendor.vendorid where Vendor.vendorid in (select vendorid from vp where productid in(select productid from Agreement where datediff(expirydate,curdate())<15))', function (error, results, fields) {
     if (error) throw error;
     var arr=results.map((val,index)=>{
         // var mailOptions = {
@@ -80,31 +80,42 @@ app.get('/', function (req, res) {
 });
 app.get('/:agreeId',(req,res)=>{
     sendnames={};
+    correct=true;
     function vendorq(){
-        connection.query(`select vendorname from Vendor where vendorid=(select vendorid from Agreement where agreementid= ${req.params.agreeId})`, (error, results, fields)=> {
+        connection.query(`select vendorname from Vendor where vendorid=(select vendorid from vp where vpid= ${req.params.agreeId})`, (error, results, fields)=> {
             if (error)
-                throw error;
-            else{
+                console.log(error);
+            else if(results[0]!=undefined){
                 results = JSON.parse(JSON.stringify(results));
                 console.log(results[0]["vendorname"]);
                 sendnames["vendorname"]=results[0]["vendorname"];
+            }
+            else{
+                correct=false;
             }
         });
     }
     
     function productq(){
-        connection.query(`select productname from Product where productid=(select productid from Agreement where agreementid= ${req.params.agreeId})`, (error, resultsp, fields) =>{
-            if (error) throw error;
-            else{
+        connection.query(`select productname from Product where productid=(select productid from vp where vpid= ${req.params.agreeId})`, (error, resultsp, fields) =>{
+            if (error) 
+            console.log(error);
+            else if(resultsp[0]!=undefined){
                 resultsp=JSON.parse(JSON.stringify(resultsp));
                 console.log(resultsp[0]["productname"]);
                 sendnames["productname"]=resultsp[0]["productname"];
+            }
+            else{
+                correct=false;
             }
         });
     }
     
     function sendq(){
-        res.render(__dirname + "/vendorForm", {name:sendnames["vendorname"]});
+        if(correct)
+            res.render(__dirname + "/vendorForm", {name:sendnames["vendorname"],agreeId:req.params.agreeId});
+        else
+            res.send("404 not found");
     }
     function executeAsynchronously(functions, timeout) {
         for(var i = 0; i < functions.length; i++) {
@@ -116,6 +127,10 @@ app.get('/:agreeId',(req,res)=>{
         executeAsynchronously(
             [vendorq, productq, sendq], 30);
     
+    
+});
+
+app.post('/:agreeId',(req,res)=>{
     
 });
 
