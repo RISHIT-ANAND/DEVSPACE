e84935581c64ba6df08ba285db49c852e988da85
Md Rushd Al Amin <72799488+vitmdrushdalamin@users.noreply.github.com>
2021-03-20 11:04:57 +0530

    Update get api.js

--- frontend/get api.js
+++ frontend/get api.js
@@ -1,6 +1,17 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://url", true);
-xhr.onload = function(){
-    console.log(xhr.responseText);
-};
-xhr.send();
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
+
+var client = new HttpClient();
+client.get('http://some/thing?with=arguments', function(response) {
+    // do something with response
+});
